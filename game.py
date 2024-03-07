c439adc6b62233313fe579d33a963a2553d76617
Shivansh Sharma <shivansh.report@gmail.com>
2024-03-07 11:14:16 +0530

    'Added Camera and View Scroll effect, Added Clouds
    # A summary of changes
    - Added a background image instead of a standard fill.
    - Added Camera Scroll for all entities
    - Fixed issue with player jump mechanics
    - Added a separate Cloud class

--- game.py
+++ game.py
@@ -6,8 +6,9 @@ import sys
 from scripts.entities import PhysicsEntity
 from scripts.utils import *
 from scripts.tilemap import Tilemap 
+from scripts.clouds import Clouds
 
-class Game:
+class Game: # Main game
 
     def __init__(self):
 
@@ -15,35 +16,50 @@ class Game:
 
         pygame.display.set_caption('Ninja Game')
 
-        self.screen = pygame.display.set_mode((640,480))
+        self.screen = pygame.display.set_mode((640,480)) # Display Window
 
-        self.display = pygame.Surface((320,240))
+        self.display = pygame.Surface((320,240)) # Main background Context
 
-        self.clock = pygame.time.Clock()
+        self.clock = pygame.time.Clock() # Update Clock
 
-        self.player = PhysicsEntity(self, 'player',(50,50),(8,15))
+         
 
-        self.movement = [False,False]
+        self.movement = [False,False] # Movement flag vectors
 
         self.assets = {
             'decor' : load_images('tiles/decor'),
             'grass' : load_images('tiles/grass'),
             'large_decor' : load_images('tiles/large_decor'),
             'stone' : load_images('tiles/stone'),
-            'player' : load_image('entities/player.png')
+            'player' : load_image('entities/player.png'),
+            'background' : load_image('background.png'),
+            'clouds' : load_images('clouds'),
         }
 
+        self.clouds = Clouds(self.assets['clouds'], count=16 )
+
+        self.player = PhysicsEntity(self, 'player',(50,50),(8,15))
+        
         self.tilemap = Tilemap(self,tile_size=16)
+
+        self.scroll = [0, 0]
 
     def run(self):
 
-        while True:
-            self.display.fill((14,219,248))
-            
-            self.tilemap.render(self.display)
+        while True: # Render tilemap, then player, then other objects 
+            self.display.blit(self.assets['background'], (0, 0))
 
-            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0)) # Update player variables
-            self.player.render(self.display) # Render the player 
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width()/ 2 - self.scroll[0]) / 10
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height()/ 2 - self.scroll[1]) / 10
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.clouds.update()
+            self.clouds.render(self.display, offset=render_scroll )
+
+            self.tilemap.render(self.display, offset=render_scroll )
+
+            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0)) # Update player variables 
+            self.player.render(self.display, offset=render_scroll ) # Render the player 
 
             print(self.tilemap.physics_rects_around(self.player.pos))
 
@@ -58,7 +74,7 @@ class Game:
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
                     if event.key == pygame.K_UP:
-                        self.player.velocity[1] = -3
+                        self.player.velocity[1] = -3 # Temporary jump button
 
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
@@ -66,6 +82,7 @@ class Game:
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
 
+            # Scale the background surface to the window context, then blit it
             self.screen.blit(pygame.transform.scale(self.display,self.screen.get_size()),(0,0))
             pygame.display.update()
 
--- scripts/entities.py
+++ scripts/entities.py
@@ -2,23 +2,23 @@ import pygame
 
 
 
-class PhysicsEntity: # Creating a general Physics Entity object
+class PhysicsEntity: # Generic Physics Entity object
 
     def __init__(self,game, e_type, pos, size): # Initializes the entity object
 
         self.game = game
-        self.type = e_type
+        self.type = e_type # Entity types will be Player, Enemies, NPCs
         self.pos = list(pos)
         self.size = size
-        self.velocity = [0,0]
+        self.velocity = [0, 0]
         self.collisions = { 'up': False, 'down': False, 'right': False, 'left':False }
 
-    def rect(self):
+    def rect(self):  # Rect object creator for entity class
 
         return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
 
 
-    def update(self,tilemap, movement=(0,0)): # Updates the entity movement
+    def update(self,tilemap, movement=(0, 0)): # Updates the entity movement
 
         self.collisions = { 'up': False, 'down': False, 'right': False, 'left':False }
         
@@ -26,13 +26,18 @@ class PhysicsEntity: # Creating a general Physics Entity object
 
         self.velocity[1] = min(5, self.velocity[1] + 0.1)
 
+        # Pretty complex way to do object collisions, 
+        # The physics entity looks in a 3x3 space around it for collision entities of interest 
+        # in the tilemap, this is why keeping the tilemap as a dictionary is an advantage
+
+        
         self.pos[0] += frame_movement[0] 
         
-        entity_rect = self.rect()
+        entity_rect = self.rect()   # Responsible for snapping the entity object and the collider object
         for rect in tilemap.physics_rects_around(self.pos):
             if entity_rect.colliderect(rect):
                 if frame_movement[0] > 0:
-                    entity_rect.right = rect.left
+                    entity_rect.right = rect.left 
                     self.collisions['right'] = True
                 if frame_movement[0] < 0:
                     entity_rect.left = rect.right
@@ -59,6 +64,6 @@ class PhysicsEntity: # Creating a general Physics Entity object
             self.velocity[1] = 0
 
 
-    def render(self,surf): # Renders the entity on the screen
+    def render(self,surf, offset=(0, 0)): # Renders the entity on the screen
 
-        surf.blit(self.game.assets['player'],self.pos)
+        surf.blit(self.game.assets['player'], (self.pos[0] - offset[0], self.pos[1] - offset[1] ))
